4a93b220d31b05475bb796fa4951a03c051273a5
sonushaikh0 <sonushaikh0457@gmail.com>
2025-09-06 13:41:28 +0530

    Update bot.js
    Signed-off-by: sonushaikh0 <sonushaikh0457@gmail.com>

--- Rudra/commands/bot.js
+++ Rudra/commands/bot.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "goibot",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "Fixed By Arun Kumar",
+  credits: "Fixed By Raja babu",
   description: "goibot",
   commandCategory: "Noprefix",
   usages: "noprefix",
@@ -22,7 +22,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
    mess = "{name}"
   if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
     var msg = {
-      body: `🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : 𝗔𝗿𝘂𝗻 𝗞𝘂𝗺𝗮𝗿🌹 `
+      body: `🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : 𝗥𝗮𝗷𝗮 𝗕𝗮𝗯𝘂🌹 `
     }
     return api.sendMessage(msg, threadID, messageID);
   };
